1333ce12685212513838e80dc1efd172b893ec44
Damien Menanteau <damien.menanteau@gmail.com>
2021-02-10 16:11:38 +0100

    Add java bootstrap files

--- java/src/main/java/RomanNumerals.java
+++ java/src/main/java/RomanNumerals.java
@@ -1,7 +1,5 @@
 class RomanNumerals {
-
-    // TODO Replace with a method more relevant for the kata
-    static int doSomething() {
-        return 42;
+    static String arabicToRoman(int arabic) {
+        return "";
     }
 }
--- java/src/test/java/RomanNumeralsTest.java
+++ java/src/test/java/RomanNumeralsTest.java
@@ -1,12 +1,15 @@
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class RomanNumeralsTest {
 
+    @Ignore
     @Test
     public void acceptance_test() {
-        // TODO Replace with an acceptance test relevant for this kata
-        assertEquals(42, RomanNumerals.doSomething());
+        int arabic = 4746;
+        String roman = "MMMMDCCXLVI";
+        assertEquals(roman, RomanNumerals.arabicToRoman(arabic));
     }
 }
