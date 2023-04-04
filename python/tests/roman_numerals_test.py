ea043f4972539d6966bc62fe2c0394fd0ae9b11e
Damien Menanteau <damien.menanteau@gmail.com>
2023-04-04 18:37:50 +0200

    Change python test framework from unittest to pytest

--- python/tests/roman_numerals_test.py
+++ python/tests/roman_numerals_test.py
@@ -1,16 +1,12 @@
-import unittest
+import pytest
 
 from roman_numerals.roman_numerals import arabic_to_roman
 
 
-class RomanNumeralsTest(unittest.TestCase):
+class TestRomanNumerals:
 
-    @unittest.skip("test currently disabled")  # Comment or remove this line to enable this test case
-    def test_acceptance_test(self):
+    @pytest.mark.skip(reason="test currently disabled")  # Comment or remove this line to enable this test case
+    def test_acceptance_test(self) -> None:
         arabic = 4746
         roman = 'MMMMDCCXLVI'
-        self.assertEqual(roman, arabic_to_roman(arabic))
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert (roman == arabic_to_roman(arabic))
